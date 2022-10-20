73df99f8cdc612da206bdbb8fb11e98c167b5293
João <106031854+cairugaJ@users.noreply.github.com>
2022-10-20 16:51:47 -0300

    2022-10-20
    Fiz os ifs que testam se a expressão é válida

--- App.java
+++ App.java
@@ -1,17 +1,102 @@
-public class App {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class App extends Metodos {
     public static void main(String args[]) {
-        StackLinkedList<String> test = new StackLinkedList<>();
+        BufferedReader reader;
+        Path path1 = Paths.get("expressoes2.txt");
+        try {
+            reader = Files.newBufferedReader(path1, Charset.defaultCharset());
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("--- Inicio expressao");
+                String v[] = line.split(" "); // divide a string pelo espaco em branco
+                StackLinkedList<String> pilha = new StackLinkedList<>();
+                for(int i = 0; i<v.length; i++){
+                    if (v[i].equals("}") || v[i].equals("]")  || v[i].equals(")")) {
+                      String op2 = pilha.pop();  
+                      String operador = pilha.pop();
+                      String op1 = pilha.pop();
+                      String abre = pilha.pop();
+                      Double res;
 
-        test.push("Primeiro Elemento Adicionado");
-        test.push("Segundo Elemento Adicionado");
-        test.push("Terceiro Elemento Adicionado");
-        test.push("Quarto Elemento Adicionado");
-        test.push("Quinto Elemento Adicionado");
+                      if (v[i].equals("}") && !abre.equals("{")){
+                        System.out.println("Erro de Sintaxe: Era esperado \"{\" no lugar de ",abre);
+                        break; //pula para a próxima expressão
+                      }
+                      if (v[i].equals("]") && !abre.equals("[")){
+                        System.out.println("Erro de Sintaxe: Era esperado \"[\" no lugar de ",abre);
+                        break; //pula para a próxima expressão
+                      }
+                      if (v[i].equals(")") && !abre.equals("(")){
+                        System.out.println("Erro de Sintaxe: Era esperado \"(\" no lugar de ",abre);
+                        break; //pula para a próxima expressão
+                      }
 
-        System.out.println(test.pop());
-        System.out.println(test.pop());
-        System.out.println(test.pop());
-        
+                      if (!String.isDigit(op2)){
+                        System.out.println("Erro de Sintaxe: Era esperado um número no lugar de ",op2);
+                        break; //pula para a próxima expressão
+                      }
+                      if (!String.isDigit(op1)){
+                        System.out.println("Erro de Sintaxe: Era esperado um número no lugar de ",op1);
+                        break; //pula para a próxima expressão
+                      }
 
+                      switch (operador){
+                        case "+": //Soma
+                            res = Double.parseDouble(op1) + Double.parseDouble(op2);
+                            Double.toString(res);
+                            v[i] = res; //aponta pro resultado da operação
+                            pilha.push(res); //empilha o resultado na pilha
+                            break;
+                        case "-": //Subtração
+                            res = Double.parseDouble(op1) - Double.parseDouble(op2);
+                            Double.toString(res);
+                            v[i] = res; //aponta pro resultado da operação
+                            pilha.push(res); //empilha o resultado na pilha
+                            break;
+                        case "*": //Multiplicação
+                            res = Double.parseDouble(op1) * Double.parseDouble(op2);
+                            Double.toString(res);
+                            v[i] = res; //aponta pro resultado da operação
+                            pilha.push(res); //empilha o resultado na pilha
+                            break;
+                        case "^": //Potenciação
+                            res = Math.pow(Double.parseDouble(op1),Double.parseDouble(op2));
+                            Double.toString(res);
+                            v[i] = res; //aponta pro resultado da operação
+                            pilha.push(res); //empilha o resultado na pilha
+                            break;
+                        default:
+                            System.out.println("Erro de Sintaxe: O operador ",operador," não é válido");
+                            break;
+                      }
+
+
+                      }
+                      
+                    
+
+                    }
+                    
+                    }
+
+                    else{
+                        pilha.push(v[i]);
+                    }
+                }
+                System.out.println("--- Fim expressao");
+            }
+            reader.close();
+         }
+          
+            catch (IOException e) {
+            System.err.format("Erro na leitura do arquivo: ", e);
+        }
     }
 }
