2d40a01899830694e0093d6dab041801a91a145b
gabicolares <gabicalicota@gmail.com>
2022-10-22 21:56:10 -0300

    apaguei coisas n usadas e coloquei na calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -13,212 +13,140 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class Calculadora {
 
-    public static void main(String[] args) {
-
+    public static void main(String args[]) {
         BufferedReader reader;
         Path path1 = Paths.get("expressoes2.txt");
         try {
             reader = Files.newBufferedReader(path1, Charset.defaultCharset());
             String line = null;
             while ((line = reader.readLine()) != null) {
-                System.out.println("--- Inicio expressao");
+
                 String v[] = line.split(" "); // divide a string pelo espaco em branco
-                ArrayList<String> expressao = new ArrayList<>();
+                StackLinkedList<String> pilha = new StackLinkedList<>();
+                System.out.print("Expressao: ");
                 for (int i = 0; i < v.length; i++) {
-                    expressao.add(v[i]);
+                    System.out.print(v[i] + " ");
                 }
-                System.out.println("--- Fim expressao");
 
-                if (verificaExpressao(expressao)) {
-                    StackLinkedList<String> pilha = new StackLinkedList<>();
-                    int tam = pilha.size();
-                    boolean auxP1 = false;
-                    boolean auxCol1 = false;
+                System.out.printf("%n %n");
+                int tamMax = pilha.size(); // inicia com o valor da pilha vazia (0)
+                boolean certo = true; // controla o print do tamanho da pilha
 
-                    // Calcula os valores entre parênteses
-                    for (int i = 0; i < expressao.size(); i++) {
+                for (int i = 0; i < v.length; i++) {
+                    if (v[i].equals("}") || v[i].equals("]") || v[i].equals(")")) {
+                        String op2 = pilha.pop();
+                        String operador = pilha.pop();
+                        String op1 = pilha.pop();
+                        String abre = pilha.pop();
+                        Double res;
+
+                        try {
+                            Double.parseDouble(op2);
+                        }
+
+                        catch (Exception e) {
+                            System.out.println("Erro de sintaxe: Era esperado um numero depois de " + op2);
+                            certo = false;
+                            break; // pula para a próxima expressão
+
+                        }
+
+                        try {
+                            Double.parseDouble(op1);
+                        }
+
+                        catch (Exception e) {
+                            System.out.println("Erro de sintaxe: Era esperado um numero depois de " + op1);
+                            certo = false;
+                            break; // pula para a próxima expressão
+
+                        }
+                        // Testa se há erro nos pares de abre e fecha
+                        if (v[i].equals(")") && !abre.equals("(") || v[i].equals("]") && !abre.equals("[")
+                                || v[i].equals("}") && !abre.equals("{")) {
+                            System.out.println("Erro de Sintaxe: Erro nos pares de abre e fecha");
+                            certo = false;
+                            break; // pula para a próxima expressão
+                        }
+
+                        // Testa se o valor do operador representa uma operação matemática válida
+                        switch (operador) {
+                            case "+": // Soma
+                                res = Double.parseDouble(op1) + Double.parseDouble(op2);
+                                pilha.push(Double.toString(res)); // empilha o resultado na pilha
+                                if (abre.equals("{")) {
+                                    System.out.printf("Resposta: %.2f%n", res);
+                                }
+                                break;
+                            case "-": // Subtração
+                                res = Double.parseDouble(op1) - Double.parseDouble(op2);
+                                // v[i] = Double.toString(res); // aponta pro resultado da operação
+                                pilha.push(Double.toString(res)); // empilha o resultado na pilha
+                                if (abre.equals("{")) {
+                                    System.out.printf("Resposta: %.2f%n", res);
+                                }
+                                break;
+                            case "*": // Multiplicação
+                                res = Double.parseDouble(op1) * Double.parseDouble(op2);
+                                // v[i] = Double.toString(res); // aponta pro resultado da operação
+                                pilha.push(Double.toString(res)); // empilha o resultado na pilha
+                                if (abre.equals("{")) {
+                                    System.out.printf("Resposta: %.2f%n", res);
+                                }
+                                break;
+                            case "/": // Divisão
+                                res = Double.parseDouble(op1) / Double.parseDouble(op2);
+                                // v[i] = Double.toString(res); // aponta pro resultado da operação
+                                pilha.push(Double.toString(res)); // empilha o resultado na pilha
+                                if (abre.equals("{")) {
+                                    System.out.printf("Resposta: %.2f%n", res);
+                                }
+                                break;
+                            case "^": // Potenciação
+                                res = Math.pow(Double.parseDouble(op1), Double.parseDouble(op2));
+                                // v[i] = Double.toString(res); // aponta pro resultado da operação
+                                pilha.push(Double.toString(res)); // empilha o resultado na pilha
+                                if (abre.equals("{")) {
+                                    System.out.printf("Resposta: %.2f%n", res);
+                                }
+                                break;
+                            default:
+                                System.out.println("Erro de Sintaxe: O operador " + operador + " não é válido");
+                                break;
+                        }
+
+                    }
+
+                    else {
                         pilha.push(v[i]);
-                        if (v[i].equals("("))
-                            auxP1 = true;
-                        if (auxP1 && pilha.top().equals(")")) {
-                            if (tam < pilha.size())
-                                tam = pilha.size();
-                            pilha.pop(); // remove o ")"
-                            String op2 = pilha.pop();
-                            System.out.println(op2);
-                            String operador = pilha.pop();
-                            System.out.println(operador);
-                            String op1 = pilha.pop();
-                            System.out.println(op1);
-                            pilha.pop(); // remove o "("
-                            pilha.push(calcula(op1, operador, op2).toString());
-                            System.out.println(calcula(op1, operador, op2).toString());
-                            auxP1 = false;
-                            expressao.remove("(");
-                            expressao.remove(op1);
-                            expressao.remove(operador);
-                            expressao.remove(op2);
-                            expressao.set(expressao.indexOf(")"), calcula(op1, operador, op2).toString());
-
-                        }
+                    }
+                    // Checa se a pilha está maior do que o valor máximo anterior
+                    if (tamMax < pilha.size()) {
+                        tamMax = pilha.size();
                     }
 
-                    // Calcula os valores entre colchetes
-                    for (int i = 0; i < expressao.size(); i++) {
-                        pilha.push(expressao.get(i));
-                        if (expressao.get(i).equals("["))
-                            auxCol1 = true;
-                        if (auxCol1 && pilha.top().equals("]")) {
-                            if (tam < pilha.size())
-                                tam = pilha.size();
-                            pilha.pop(); // remove o "]"
-                            String op2 = pilha.pop();
-                            System.out.println(op2);
-                            String operador = pilha.pop();
-                            System.out.println(operador);
-                            String op1 = pilha.pop();
-                            System.out.println(op1);
-                            pilha.pop(); // remove o "["
-                            pilha.push(calcula(op1, operador, op2).toString());
-                            System.out.println(calcula(op1, operador, op2).toString());
-                            auxCol1 = false;
+                }
 
-                        }
-                    }
-
-                    pilha.pop(); // remove o "}"
-                    String op2 = pilha.pop();
-                    String operador = pilha.pop();
-                    String op1 = pilha.pop();
-                    pilha.pop(); // remove o "{"
-                    double res = calcula(op1, operador, op2);
-
-                    String exp = "";
-                    for (int i = 0; i < expressao.size(); i++) {
-                        exp += expressao.get(i) + " ";
-                    }
-                    System.out.println("Expressao" + exp);
-                    System.out.printf("Resultado: %.2f\n", res);
-                    System.out.printf("Tamanho máximo da pilha: %d\n", tam);
-
+                if (certo == true) {
+                    System.out.printf(
+                            "Tamanho máximo da pilha: %d\n---------------------------------------------------------------------\n",
+                            tamMax);
                 }
 
                 else {
-                    System.out.print("Erro de sintaxe:");
-                    verificaExpressao(expressao);
+                    System.out.printf("---------------------------------------------------------------------\n");
                 }
             }
+
             reader.close();
-        } catch (IOException e) {
+
+        }
+
+        catch (IOException e) {
             System.err.format("Erro na leitura do arquivo: ", e);
         }
-
     }
-
-    /**
-     * Verifica se a expressão passada como parâmetro é válida
-     * 
-     * @param expressao a ser analisada
-     * @return true caso ela seja válida e false + mensagem de erro caso seja
-     *         inválida
-     * @version 2022-10-17
-     */
-    public static boolean verificaExpressao(ArrayList<String> expressao) {
-        int contadorParenteses = 0;
-        int contadorChaves = 0;
-        int contadorColchetes = 0;
-
-        for (int i = 0; i < expressao.size(); i++) {
-            if (expressao.get(i).equals("(")) {
-                contadorParenteses++;
-            }
-
-            if (expressao.get(i).equals(")")) {
-                contadorParenteses--;
-            }
-
-            if (expressao.get(i).equals("[")) {
-                contadorColchetes++;
-            }
-
-            if (expressao.get(i).equals("]")) {
-                contadorColchetes--;
-            }
-
-            if (expressao.get(i).equals("{")) {
-                contadorChaves++;
-            }
-
-            if (expressao.get(i).equals("}")) {
-                contadorChaves--;
-            }
-        }
-
-        if (contadorParenteses == 0 && contadorChaves == 0 && contadorColchetes == 0) {
-            return true;
-        }
-
-        else if (contadorParenteses < 0) {
-            System.out.printf("Existe(m) %d fechamento(s) de parênteses sem abertura\n", contadorParenteses * -1);
-            return false;
-        }
-
-        else if (contadorParenteses > 0) {
-            System.out.printf("Existe(m) %d abertura(s) de parênteses sem fechamento\n", contadorParenteses);
-            return false;
-        }
-
-        else if (contadorColchetes < 0) {
-            System.out.printf("Existe(m) %d fechamento(s) de colchetes sem abertura\n", contadorColchetes * -1);
-            return false;
-        }
-
-        else if (contadorColchetes > 0) {
-            System.out.printf("Existe(m) %d abertura(s) de colchetes sem fechamento\n", contadorColchetes);
-            return false;
-        }
-
-        else if (contadorChaves < 0) {
-            System.out.printf("Existe(m) %d fechamento(s) de chaves sem abertura\n", contadorChaves * -1);
-            return false;
-        }
-
-        else if (contadorChaves > 0) {
-            System.out.printf("Existe(m) %d abertura(s) de chaves sem fechamento\n", contadorChaves);
-            return false;
-        }
-        return false;
-    }
-
-    /**
-     *
-     * Método que realiza os cálculos
-     * 
-     * @param x,op,y operando 1, operador, operando 2
-     * @return resultado do cálculo
-     *
-     * @version 2022-10-17
-     */
-    public static Double calcula(String x, String op, String y) {
-        double res = 0;
-
-        if (op.equals("+")) {
-            res = Double.parseDouble(x) + Double.parseDouble(y);
-        } else if (op.equals("-")) {
-            res = Double.parseDouble(x) - Double.parseDouble(y);
-        } else if (op.equals("*")) {
-            res = Double.parseDouble(x) * Double.parseDouble(y);
-        } else if (op.equals("/")) {
-            res = Double.parseDouble(x) / Double.parseDouble(y);
-        } else if (op.equals("^")) {
-            res = Math.pow(Double.parseDouble(x), Double.parseDouble(y));
-        }
-
-        return res;
-    }
-
 }
